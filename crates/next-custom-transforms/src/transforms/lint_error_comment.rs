78aa405c8c43fb7c432acef391f6a4c0033deb84
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-10 23:04:25 +0900

    Add lint_error_comment.rs

---
+++ crates/next-custom-transforms/src/transforms/lint_error_comment.rs
@@ -0,0 +1,51 @@
+use swc_core::{
+    common::{
+        comments::{Comment, Comments},
+        errors::HANDLER,
+    },
+    ecma::{utils::swc_common::Span, visit::Visit},
+};
+
+struct LintErrorComment<C>
+where
+    C: Comments,
+{
+    comments: C,
+}
+
+pub fn lint_error_comment<C>(comments: C) -> impl Vsiit
+where
+    C: Comments,
+{
+    LintErrorComment { comments }
+}
+
+impl<C> LintErrorComment<C>
+where
+    C: Comments,
+{
+    fn lint(&self, comment: &Comment, is_leading: bool) {
+        // TODO: Check content of comment and report error if it's an error comment
+    }
+}
+
+fn report(span: Span, msg: &str) {
+    HANDLER.with(|handler| {
+        handler.struct_span_err(span, &msg).emit();
+    })
+}
+
+impl<C> Visit for LintErrorComment<C>
+where
+    C: Comments,
+{
+    fn visit_span(&mut self, s: &Span) {
+        self.comments.with_leading(s.lo, |c| {
+            self.lint(c, true);
+        });
+
+        self.comments.with_trailing(s.hi, |c| {
+            self.lint(c, false);
+        });
+    }
+}
