1f3a98494a51bfae92f3bf853eb8d1f5e7ee9929
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-10-15 14:59:41 +0900

    trivial impls
    WIP: codegen
    
    has_top_level_await
    
    __turbopack_import__
    
    `__turbopack_export_namespace__(__turbopack_import__({}))`

--- turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
+++ turbopack/crates/turbopack-ecmascript/src/references/esm/export.rs
@@ -22,6 +22,7 @@ use turbopack_core::{
     issue::{analyze::AnalyzeIssue, IssueExt, IssueSeverity, StyledString},
     module::Module,
     reference::ModuleReference,
+    resolve::ModulePart,
 };
 
 use super::base::ReferencedAsset;
@@ -29,6 +30,7 @@ use crate::{
     chunk::{EcmascriptChunkPlaceable, EcmascriptExports},
     code_gen::{CodeGenerateable, CodeGeneration, CodeGenerationHoistedStmt},
     magic_identifier,
+    tree_shake::asset::EcmascriptModulePartAsset,
 };
 
 #[derive(Clone, Hash, Debug, PartialEq, Eq, Serialize, Deserialize, TraceRawVcs)]
